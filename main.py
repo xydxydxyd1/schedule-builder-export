a8c75632acd2dcb57db0853cdd61791cd84b9fa7
Eric Xu <xydxydxyd1@gmail.com>
2025-01-03 20:59:56 -0800

    feat: parse events

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # Export schedule builder input
 from pathlib import Path
-from icalendar import Event
+from icalendar import Event, vText
 from typing import NamedTuple
+from datetime import datetime
 import json
 import pprint
+
+# maps format from response to ics standard
+BYDAY_MAPPING = {
+    'M': 'MO',
+    'T': 'TU',
+    'W': 'WE',
+    'R': 'TH',
+    'F': 'FR',
+}
 
 export_path = Path("exported.ics")
 
@@ -14,14 +24,50 @@ def parse(classinfo) -> list[Event]:
     """Parse classinfo into a list of events
 
     Args:
-    classinfo - The parsed json response of search.cfc request
+    classinfo - Dictionary containing information of one class. Format follows
+    the search.cfc request.
     """
+
     events: list[Event] = []
+
+    classname = classinfo['course']['shortDesc']
+    desc = classinfo['icmsData']['newDescription']
+
+    for meeting in classinfo['meeting']:
+        event = Event()
+        event.add('description', desc)
+        event.add('summary', f'{classname} - {meeting['description']}')
+
+        dtstart = datetime.strptime(meeting['startDate'], '%B, %d %Y %H:%M:%S')
+        dtstart.replace(hour=int(meeting['startTime'][:2]),
+                        minute=int(meeting['startTime'][2:4]))
+        event.add('dtstart', dtstart)
+        dtend = datetime.strptime(meeting['endDate'], '%B, %d %Y %H:%M:%S')
+        dtend.replace(hour=int(meeting['endTime'][:2]),
+                        minute=int(meeting['endTime'][2:4]))
+        event.add('dtend', dtend)
+        event.add('dtstamp', datetime.now())
+
+        byday = []
+        for c in meeting['daysString']:
+            byday.append(BYDAY_MAPPING[c])
+        byday = ','.join(byday)
+        event.add('rrule', {
+            'freq': 'weekly',
+            'interval': 1,
+            'byday': byday
+        })
+
+        event.add('location', f'{meeting['room']} {meeting['building']}')
+
+        pprint.pp(meeting)
+        print(event)
+        events.append(event)
     return events
 
 if __name__ == "__main__":
     pprint.pp(course_info)
-    print(course_info[0]['icmsData']['newDescription'])
+    parse(course_info[0])
 #    parsed_class_info = parse(class_info)
 #    calendar = get_calendar(parsed_class_info)
 #    write_export(path, calendar)
