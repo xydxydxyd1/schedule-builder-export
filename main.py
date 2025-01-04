cab5caa40d3c98f23cc4fed070a47ffe48191ba7
Eric Xu <xydxydxyd1@gmail.com>
2025-01-03 22:11:10 -0800

    fix: dtstart and dtend is the first occurrence

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import tempfile, os
 from icalendar import Calendar, Event
 from typing import NamedTuple
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 import pprint
 
@@ -15,11 +15,29 @@ BYDAY_MAPPING = {
     'R': 'TH',
     'F': 'FR',
 }
+# maps format from response to python datetime standard
+DATETIME_MAPPING = {
+    'M': 0,
+    'T': 1,
+    'W': 2,
+    'R': 3,
+    'F': 4,
+}
 
 export_path = Path("exported.ics")
 
 course_info = '[{"courseDepartmentNotes":[],"courseSubjectNotes":[],"icmsData":{"newDescription":"Theory, application, and design of analog circuits. Methods of analysis including frequency response, SPICE simulation, and Laplace transform. Operational amplifiers and design of active filters.  ","creditLimit":"Students who have completed ENG 100 may receive 3.5 units of credit.","ge3":"QL,SE,VL","crossListing":"","prereq":" ENG 017 C- or better;  (MAT 022B or MAT 027B)"},"finalExam":{"examDate":"March, 19 2025 13:00:00"},"meeting":[{"thursday":false,"sunday":false,"startDate":"January, 06 2025 00:00:00","endTime":"2100","tuesday":true,"friday":false,"endDate":"March, 14 2025 00:00:00","daysString":"T","wednesday":false,"monday":false,"meetCode":"C","startTime":"1810","building":"Kemper Hall","description":"Laboratory","buildingCode":"KEMPER","saturday":false,"type":"LAB","room":"02161"},{"thursday":true,"sunday":false,"startDate":"January, 06 2025 00:00:00","endTime":"1730","tuesday":true,"friday":false,"endDate":"March, 14 2025 00:00:00","daysString":"TR","wednesday":false,"monday":false,"meetCode":"A","startTime":"1610","building":"Olson Hall","description":"Lecture","buildingCode":"OLSON","saturday":false,"type":"LEC","room":"00006"},{"thursday":true,"sunday":false,"startDate":"January, 06 2025 00:00:00","endTime":"1800","tuesday":true,"friday":false,"endDate":"March, 14 2025 00:00:00","daysString":"TR","wednesday":false,"monday":false,"meetCode":"D","startTime":"1740","building":"Olson Hall","description":"Discussion","buildingCode":"OLSON","saturday":false,"type":"DIS","room":"00217"}],"crnNotes":[],"importantNotes":[],"course":{"termCode":"202501","dropDesc":"10 Day Drop","unitsLow":5,"reservedSeating":false,"shortDesc":"EEC 100 A02","gradeMode":"","unitsInd":"","seqNum":"A02","printCRN":"20411","unitsHigh":0,"dropDate":"January, 17 2025 00:00:00","title":"Circuits II","subjectDesc":"Engineering Electrical & Compu","courseNum":"100","hidCRN":"20411","crn":"20411","subjectCode":"EEC"},"instructor":[{"lastName":"Kolner","instructorName":"B. Kolner","firstName":"Brian","instructorEmail":"bhkolner@ucdavis.edu","fullName":"Brian Kolner"}]}]'
 course_info = json.loads(course_info)
+
+def next_weekday(d, weekdays):
+    """Change `d` to the next weekday in `weekdays`. If currenday matches,
+    return `d` without modification"""
+    min_days_ahead = 8
+    for weekday in weekdays:
+        days_ahead = weekday - d.weekday()
+        if days_ahead < min_days_ahead:
+            min_days_ahead = days_ahead
+    return d + timedelta(min_days_ahead)
 
 def parse(classinfo) -> list[Event]:
     """Parse classinfo into a list of events
@@ -39,18 +57,7 @@ def parse(classinfo) -> list[Event]:
         event.add('description', desc)
         event.add('summary', f'{classname} - {meeting['description']}')
 
-        # dtstart and end has the same date but not the same time; endDate
-        # refers to the last class, accounting for repetition
-        dtstart = datetime.strptime(meeting['startDate'], '%B, %d %Y %H:%M:%S')
-        dtend = dtstart
-        dtstart = dtstart.replace(hour=int(meeting['startTime'][:2]),
-                        minute=int(meeting['startTime'][2:4]))
-        dtend = dtend.replace(hour=int(meeting['endTime'][:2]),
-                        minute=int(meeting['endTime'][2:4]))
-        event.add('dtstart', dtstart)
-        event.add('dtend', dtend)
-        event.add('dtstamp', datetime.now())
-
+        # Recurrence
         until = datetime.strptime(meeting['endDate'], '%B, %d %Y %H:%M:%S')
         byday = []
         for c in meeting['daysString']:
@@ -63,9 +70,27 @@ def parse(classinfo) -> list[Event]:
             'until': until
         })
 
+        # First occurrence start/end datetime
+        byday = []
+        for c in meeting['daysString']:
+            byday.append(DATETIME_MAPPING[c])
+        # dtstart and end has the same date but not the same time; endDate
+        # refers to the last class, accounting for repetition
+        dtstart = datetime.strptime(meeting['startDate'], '%B, %d %Y %H:%M:%S')
+        dtstart = next_weekday(dtstart, byday)
+        dtend = dtstart
+        dtstart = dtstart.replace(hour=int(meeting['startTime'][:2]),
+                        minute=int(meeting['startTime'][2:4]))
+        dtend = dtend.replace(hour=int(meeting['endTime'][:2]),
+                        minute=int(meeting['endTime'][2:4]))
+        event.add('dtstart', dtstart)
+        event.add('dtend', dtend)
+        event.add('dtstamp', datetime.now())
+
         event.add('location', f'{meeting['room']} {meeting['building']}')
 
         events.append(event)
+    print(events[0])
     return events
 
 if __name__ == "__main__":
